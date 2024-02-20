388507d2f1d59615541a7ee23334d80630cb90d3
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-20 09:20:39 -0300

    Lesson 2 - Forwarding attributes

--- resources/views/components/separator.blade.php
+++ resources/views/components/separator.blade.php
@@ -1,10 +1,14 @@
-@props([
-    'vertical' => false,
-])
+    @props([
+        'vertical' => false,
+    ])
 
-@if ($vertical)
-    <div aria-orientation="vertical" role="separator" class="bg-gray-400 h-full w-[1px]"></div>
-@else
-    <div aria-orientation="horinzontal" role="separator" class="bg-gray-400 h-[1px] w-full"></div>
-@endif
+    <div
+        role="separator"
+        aria-orientation="{{ $vertical ? 'vertical' : 'horizontal' }}"
+        {{ $attributes->class([
+            'bg-gray-400',
+            'h-full w-[1px]' => $vertical,
+            'h-[1px] w-full' => ! $vertical,
+        ]) }}
+    ></div>
 
