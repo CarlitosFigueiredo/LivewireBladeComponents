5b6f0764542065a41e6155e03f074d8b5991177b
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-20 11:52:14 -0300

    Lesson 7 - Condicional attributes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,51 @@
 <x-layouts.app>
-    <div class="grid gap-4">
+    <div class="grid gap-8">
+        <div class="grid gap-4">
+            <h2 class="text-5xl font-bold">Money Input</h2>
 
-        <h2 class="text-5xl font-bold">Sepparator</h2>
+            <x-separator />
 
-        <x-separator />
-
-        <div class="flex gap-4">
-            <div>Home</div>
-
-            <x-separator vertical />
-
-            <div>About</div>
-
-            <x-separator vertical />
-
-            <div>Team</div>
+            <x-input.money placeholder="0.00" class="!border-red-500" />
         </div>
-    </div>
 
-    <div class="grid gap-4">
+        <div class="grid gap-4">
 
-        <h2 class="text-5xl font-bold">Card</h2>
+            <h2 class="text-5xl font-bold">Sepparator</h2>
 
-        <x-separator />
+            <x-separator />
 
-        <div class="flex gap-4">
+            <div class="flex gap-4">
+                <div>Home</div>
 
-            {{-- Card --}}
-            <x-card heading="Skateboards" slim>
-                <p>Shop our line of custom-made skate decks. Each deck is hand-crafted form lumber grown in Loompa land.</p>
+                <x-separator vertical />
 
-                <x-card.footer>
+                <div>About</div>
 
-                    <button class="rounded-lg bg-gray-300 hover:bg-gray-400/50 py-1.5 px-6 flex-grow">Shop now</button>
-                    <button class="rounded-lg bg-gray-300 hover:bg-gray-400/50 py-1.5 px-6 flex-grow">Shop later</button>
-                </x-card.footer>
-            </x-card>
+                <x-separator vertical />
+
+                <div>Team</div>
+            </div>
+        </div>
+
+        <div class="grid gap-4">
+
+            <h2 class="text-5xl font-bold">Card</h2>
+
+            <x-separator />
+
+            <div class="flex gap-4">
+
+                {{-- Card --}}
+                <x-card heading="Skateboards" slim>
+                    <p>Shop our line of custom-made skate decks. Each deck is hand-crafted form lumber grown in Loompa land.</p>
+
+                    <x-card.footer>
+
+                        <button class="rounded-lg bg-gray-300 hover:bg-gray-400/50 py-1.5 px-6 flex-grow">Shop now</button>
+                        <button class="rounded-lg bg-gray-300 hover:bg-gray-400/50 py-1.5 px-6 flex-grow">Shop later</button>
+                    </x-card.footer>
+                </x-card>
+            </div>
         </div>
     </div>
 </x-layouts.app>
